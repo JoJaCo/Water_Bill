16d511ea66479595b28f644d989bab6c423f3a9c
JoJaCo <contrerasjorge226@gmail.com>
2021-02-16 17:28:36 -0800

    added logic to check if the user enterd his/her info

--- app/src/main/java/com/calivera/andriod/waterbill/MainActivity.kt
+++ app/src/main/java/com/calivera/andriod/waterbill/MainActivity.kt
@@ -2,10 +2,28 @@ package com.calivera.andriod.waterbill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener(){
+            if (account_number.text.toString().isEmpty()){
+                Toast.makeText(
+                    this,
+                    "please enter your Account Number",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                Toast.makeText(this,
+                "you entered ${account_number.text.toString()} as your Account Number",
+                Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
